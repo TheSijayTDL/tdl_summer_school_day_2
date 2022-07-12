1688126efe1c0ad680ebbf71d9b74d73b811a190
TheSijayTDL <109088219+TheSijayTDL@users.noreply.github.com>
2022-07-12 15:42:28 +0300

    Update progressBar.cy.js

--- cypress/e2e/demo-qa/progressBar.cy.js
+++ cypress/e2e/demo-qa/progressBar.cy.js
@@ -7,15 +7,15 @@ context("Progress Bar", () => {
 
     it("Progress Bar scenario", () => {
       progressBar.startBar.click();
-      if(cy.get("#progressBar").should("include.text", "25")) {
+      if(cy.get("#progressBar").should("include.text", "25%")) {
           progressBar.startBar.click();
           progressBar.startBar.wait(1500).click();
       }
-      if(cy.get("#progressBar").should("include.text", "75")) {
+      if(cy.get("#progressBar").should("include.text", "75%")) {
           progressBar.startBar.click();
           progressBar.startBar.wait(1500).click();
       }
-      if(cy.get("#progressBar").should("include.text", "99")) {
+      if(cy.get("#progressBar").should("include.text", "99%")) {
           progressBar.startBar.click();
       }
     });
